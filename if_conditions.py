a2f584951aeb795763ac9d204eb536feb6999bf9
Samuel Ige <samuelige.iyanu@gmail.com>
2024-01-16 17:23:16 +0100

    add - if conditions

---
+++ if_conditions.py
@@ -0,0 +1,20 @@
+# Declaring variables
+x = 3
+
+# IF Statement
+if x > 5:
+    print("X is greater than 5")
+
+# IF + else Statement
+if x >= 3:
+    print("X is greater or equal to 3")
+else: 
+    print("X is less than 5")
+
+# IF + elif Statement
+if x > 5:
+    print("X is greater than 5")
+elif x > 2 and x < 4: 
+    print("X is between 2 and 4")
+else: 
+    print(x)
