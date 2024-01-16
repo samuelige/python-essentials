28b5e13293ea59ca8c2bd36e5bc9a87496f5991e
Samuel Ige <samuelige.iyanu@gmail.com>
2024-01-16 01:28:32 +0100

    add - input function

---
+++ input_function.py
@@ -0,0 +1,5 @@
+# Display a custom prompt to guide users by passing a string argument to the input() function:
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+
+print(f"My name is {name}. And I'm {age} years old")
