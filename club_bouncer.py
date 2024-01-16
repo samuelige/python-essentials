b0196a7fb4ff48d357588439b56d99ba0a9250a2
Samuel Ige <samuelige.iyanu@gmail.com>
2024-01-16 19:08:44 +0100

    add - club bouncer challenge

---
+++ club_bouncer.py
@@ -0,0 +1,28 @@
+# Challence
+# You are the bouncer at the entrance of Berghain nightclub. The club has the following rules for entry:
+# Guests must be at least 18 years old.
+# Guests must not be older than 60 years old.
+# The maximum number of guests allowed in the club is 200.
+# Using print and input functions, prompt the user for their age and the current number of guests inside the club. 
+# Apply arithmetic operations and comparison operators to determine if the guest can enter the club.
+# If the guest is allowed to enter, print a message saying, "Welcome to Berghain!"
+# If the guest is not allowed to enter due to age restrictions, print a message saying, "Sorry, you cannot enter the
+# club due to age restrictions."
+# If the guest is not allowed to enter due to the club being at maximum capacity, print a message saying, "Sorry,
+# the club is at maximum capacity. Please try again later."
+
+# Solution
+
+# maximum capacity
+max_capacity = 200
+
+# Using print and input functions, prompt the user for their age and the current number of guests inside the club. 
+guest = int(input("How old are you?  "))
+
+# Guests must be at least 18 years old.
+if guest < 18 or guest >= 60:
+    print("Sorry, you cannot enter the club due to age restrictions.")
+elif max_capacity >= 200:
+    print("sorry, the club is at maximum capacity. Please try again later.")
+else:
+    print("Welcome to Berghain!")
