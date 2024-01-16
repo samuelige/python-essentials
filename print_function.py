ea5004a5831d9b8e7883a413e4784619cef800ae
Samuel Ige <samuelige.iyanu@gmail.com>
2024-01-16 01:12:18 +0100

    add - print function

--- print_function.py
+++ print_function.py
@@ -1,2 +1,12 @@
 # Simple print example
 print("Hello")
+
+
+name = "Samuel"
+age= 31
+
+# String concatenation (+)
+print ("My name is " + name + " and age is " + str(age) )
+
+# F-string
+print(f"my name is {name} and I am {age} years old")
