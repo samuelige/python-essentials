2c1c2eb51aaf0174d5d30a962067ec54310aa4a0
Samuel Ige <samuelige.iyanu@gmail.com>
2024-01-16 12:06:39 +0100

    add - arithmetic operators

---
+++ arithmetic_operators.py
@@ -0,0 +1,36 @@
+# Declaring variables
+x = 5
+y = 10
+
+# Addition
+result_add = x + y
+print(result_add)
+
+# Subtractions
+result_subtract = x - y
+print(result_subtract)
+
+# Multiplication
+result_multiply = x * y
+print(result_multiply)
+
+# Division
+result_divide = x / y
+print(result_divide)
+
+# Modulus
+result_remainder = x % y
+print(result_remainder)
+
+# Exponential
+result_exponential = x ** y
+print(result_exponential)
+
+# Floor Division
+result_floorDivide = x // y
+print(result_floorDivide)
+
+# Compound Assignment Operators (+=, -=, *=, /=, %=, **=, //=)
+x += 3 # Equivalent to x = x + y
+print(x)
+
---
+++ variable_types.py
@@ -0,0 +1,17 @@
+
+name = "Samuel"
+age = 10
+height = 1.67
+
+# Checking Variable Type
+print(type(name))
+print(type(age))
+print(type(height))
+
+# Transform a float into an integer
+height_integer = int(height)
+print(height_integer)
+
+# Transform a integer into an float
+height_float = float(age)
+print(height_float)
