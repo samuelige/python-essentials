7c80ce7651d090e4f67d2fe29f196624e49c3033
Samuel Ige <samuelige.iyanu@gmail.com>
2024-01-16 12:28:38 +0100

    add - comparison operators

---
+++ comparison_operators.py
@@ -0,0 +1,27 @@
+# Declaring variables
+x = 5
+y = 10
+
+# Equal to (==)
+result_qualTo = x == y
+print(result_qualTo)
+
+# Not Equal
+result_notEqual = x != y
+print(result_notEqual)
+
+# Greater than
+result_greaterThan = x > y
+print(result_greaterThan)
+
+# Less than
+result_lessThan = x < y
+print(result_lessThan)
+
+# Greater than or equal to
+result_greaterThanOrEqualTo = x >= y
+print(result_greaterThanOrEqualTo)
+
+# Less than or equal to
+result_lessThanOrEqualTo = x <= y
+print(result_lessThanOrEqualTo)
