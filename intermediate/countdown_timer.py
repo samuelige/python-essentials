5181f57b83038744ec51d12da7ef2ad95a8bf617
Samuel Ige <samuelige.iyanu@gmail.com>
2024-01-18 03:24:16 +0100

    add - count down exercise

---
+++ intermediate/countdown_timer.py
@@ -0,0 +1,11 @@
+# Exercise: Create a countdown time from 10 to 0
+
+countdown = 10
+
+while countdown >= 0:
+    if countdown == 0:
+        print("Timer completed")
+    else:
+        print(countdown)
+
+    countdown -= 1
