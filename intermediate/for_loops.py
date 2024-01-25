e5e623c165b48e812569a2c3365598e2a0d7d9a1
Samuel Ige <samuelige.iyanu@gmail.com>
2024-01-25 15:52:17 +0100

    add - for loops

---
+++ intermediate/for_loops.py
@@ -0,0 +1,10 @@
+colors = ['red', 'green', 'blue', 'yellow']
+
+for item in colors:
+    print(f"The color is {item}")
+
+# Using the range function
+# list(range(1, 5))
+    
+for i in range(5):
+    print(f"The range is {i}")
